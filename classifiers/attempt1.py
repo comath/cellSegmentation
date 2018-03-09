25fb748dd00d174a026858430aee3e31ffb35ef0
Sven <scattell@gmail.com>
2018-03-08 20:20:20 -0500

    first commit

---
+++ classifiers/attempt1.py
@@ -0,0 +1,133 @@
+'''
+Boilerplate copied from here:
+https://www.kaggle.com/kmader/nuclei-overview-to-submission
+'''
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from glob import glob
+import os
+from skimage.io import imread
+import matplotlib.pyplot as plt
+
+
+dsb_data_dir = os.path.join('..','input')
+stage_label = 'stage1'
+
+
+train_labels = pd.read_csv(os.path.join(dsb_data_dir,'{}_train_labels.csv'.format(stage_label)))
+train_labels['EncodedPixels'] = train_labels['EncodedPixels'].map(lambda ep: [int(x) for x in ep.split(' ')])
+
+
+all_images = glob(os.path.join(dsb_data_dir, 'stage1_*', '*', '*', '*'))
+img_df = pd.DataFrame({'path': all_images})
+img_id = lambda in_path: in_path.split('/')[-3]
+img_type = lambda in_path: in_path.split('/')[-2]
+img_group = lambda in_path: in_path.split('/')[-4].split('_')[1]
+img_stage = lambda in_path: in_path.split('/')[-4].split('_')[0]
+img_df['ImageId'] = img_df['path'].map(img_id)
+img_df['ImageType'] = img_df['path'].map(img_type)
+img_df['TrainingSplit'] = img_df['path'].map(img_group)
+img_df['Stage'] = img_df['path'].map(img_stage)
+
+train_df = img_df.query('TrainingSplit=="train"')
+train_rows = []
+group_cols = ['Stage', 'ImageId']
+for n_group, n_rows in train_df.groupby(group_cols):
+    c_row = {col_name: col_value for col_name, col_value in zip(group_cols, n_group)}
+    c_row['masks'] = n_rows.query('ImageType == "masks"')['path'].values.tolist()
+    c_row['images'] = n_rows.query('ImageType == "images"')['path'].values.tolist()
+    train_rows += [c_row]
+train_img_df = pd.DataFrame(train_rows)    
+IMG_CHANNELS = 3
+def read_and_stack(in_img_list):
+    return np.sum(np.stack([imread(c_img) for c_img in in_img_list], 0), 0)/255.0
+train_img_df['images'] = train_img_df['images'].map(read_and_stack).map(lambda x: x[:,:,:IMG_CHANNELS])
+train_img_df['masks'] = train_img_df['masks'].map(read_and_stack).map(lambda x: x.astype(int))
+
+n_img = 6
+fig, m_axs = plt.subplots(2, n_img, figsize = (12, 4))
+for (_, c_row), (c_im, c_lab) in zip(train_img_df.sample(n_img).iterrows(), 
+                                     m_axs.T):
+    c_im.imshow(c_row['images'])
+    c_im.axis('off')
+    c_im.set_title('Microscope')
+    
+    c_lab.imshow(c_row['masks'])
+    c_lab.axis('off')
+    c_lab.set_title('Labeled')
+
+from keras.models import Sequential
+from keras.layers import BatchNormalization, Conv2D, UpSampling2D, Lambda
+simple_cnn = Sequential()
+simple_cnn.add(BatchNormalization(input_shape = (None, None, IMG_CHANNELS), 
+                                  name = 'NormalizeInput'))
+simple_cnn.add(Conv2D(8, kernel_size = (3,3), padding = 'same'))
+simple_cnn.add(Conv2D(8, kernel_size = (3,3), padding = 'same'))
+# use dilations to get a slightly larger field of view
+simple_cnn.add(Conv2D(16, kernel_size = (3,3), dilation_rate = 2, padding = 'same'))
+simple_cnn.add(Conv2D(16, kernel_size = (3,3), dilation_rate = 2, padding = 'same'))
+simple_cnn.add(Conv2D(32, kernel_size = (3,3), dilation_rate = 3, padding = 'same'))
+
+# the final processing
+simple_cnn.add(Conv2D(16, kernel_size = (1,1), padding = 'same'))
+simple_cnn.add(Conv2D(1, kernel_size = (1,1), padding = 'same', activation = 'sigmoid'))
+print simple_cnn.summary()
+
+from keras import backend as K
+smooth = 1.
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+simple_cnn.compile(optimizer = 'adam', 
+                   loss = dice_coef_loss, 
+                   metrics = [dice_coef, 'acc', 'mse'])
+
+def simple_gen():
+    while True:
+        for _, c_row in train_img_df.iterrows():
+            yield np.expand_dims(c_row['images'],0), np.expand_dims(np.expand_dims(c_row['masks'],-1),0)
+
+simple_cnn.fit_generator(simple_gen(), 
+                         steps_per_epoch=train_img_df.shape[0],
+                        epochs = 3)
+
+test_df = img_df.query('TrainingSplit=="test"')
+test_rows = []
+group_cols = ['Stage', 'ImageId']
+for n_group, n_rows in test_df.groupby(group_cols):
+    c_row = {col_name: col_value for col_name, col_value in zip(group_cols, n_group)}
+    c_row['images'] = n_rows.query('ImageType == "images"')['path'].values.tolist()
+    test_rows += [c_row]
+test_img_df = pd.DataFrame(test_rows)    
+
+test_img_df['images'] = test_img_df['images'].map(read_and_stack).map(lambda x: x[:,:,:IMG_CHANNELS])
+print(test_img_df.shape[0], 'images to process')
+
+
+test_img_df['masks'] = test_img_df['images'].map(lambda x: simple_cnn.predict(np.expand_dims(x, 0))[0, :, :, 0])
+
+n_img = 3
+from skimage.morphology import closing, opening, disk
+def clean_img(x):
+    return opening(closing(x, disk(1)), disk(3))
+fig, m_axs = plt.subplots(3, n_img, figsize = (12, 6))
+for (_, d_row), (c_im, c_lab, c_clean) in zip(test_img_df.sample(n_img).iterrows(), 
+                                     m_axs):
+    c_im.imshow(d_row['images'])
+    c_im.axis('off')
+    c_im.set_title('Microscope')
+    
+    c_lab.imshow(d_row['masks'])
+    c_lab.axis('off')
+    c_lab.set_title('Predicted')
+    
+    c_clean.imshow(clean_img(d_row['masks']))
+    c_clean.axis('off')
+    c_clean.set_title('Clean')
+
+plt.show()
